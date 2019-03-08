52d552b6214a40368686c04d9487c09316c81424
Mountain1210 <works07@126.com>
2019-03-08 15:39:46 +0800

    ce

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
   <NavHeader></NavHeader>
   <div id="mainContent">
-   <!-- <NavLeft></NavLeft> -->
+   <!-- <NavLeft>123132</NavLeft> -->
     <router-view/>
     </div>
   </div>
